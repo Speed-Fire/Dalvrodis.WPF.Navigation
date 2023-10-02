f354a1594f316bad46421827f933a5e84d32b3f8
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-02 19:33:03 +0200

    Adding INavigationService.

---
+++ Synergy.WPF.Navigation/Services/INavigationService.cs
@@ -0,0 +1,16 @@
+﻿using Synergy.WPF.Navigation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.WPF.Navigation.Services
+{
+    public interface INavigationService
+    {
+        ViewModel? CurrentView { get; }
+        void NavigateTo<TViewModel>() where TViewModel : ViewModel;
+        void NavigateTo(ViewModel viewModel);
+    }
+}
