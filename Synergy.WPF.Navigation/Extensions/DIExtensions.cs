28662521e6191ee2a595f6419760e23e3010e0b1
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-02 19:36:28 +0200

    Adding DI extensions.

---
+++ Synergy.WPF.Navigation/Extensions/DIExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Synergy.WPF.Navigation.Services;
+using Synergy.WPF.Navigation.Services.Global;
+using Synergy.WPF.Navigation.Services.Local;
+using Synergy.WPF.Navigation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.WPF.Navigation.Extensions
+{
+    public static class DIExtensions
+    {
+        public static IServiceCollection RegisterNavigation(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<INavigationService, NavigationService>()
+                .AddSingleton<Func<Type, ViewModel>>(provider => vmType => (ViewModel)provider.GetRequiredService(vmType))
+                .AddTransient<ILocalNavigationService, LocalNavigationService>();
+
+            return services;
+        }
+    }
+}
