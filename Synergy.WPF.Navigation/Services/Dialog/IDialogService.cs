c900077615ca0f1389123c617def9b62831033eb
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-11-11 22:02:09 +0100

    Adding base things for dialogs.

---
+++ Synergy.WPF.Navigation/Services/Dialog/IDialogService.cs
@@ -0,0 +1,19 @@
+﻿using Synergy.WPF.Navigation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Synergy.WPF.Navigation.Services.Dialog
+{
+	public delegate void DialogCallback(bool? result);
+	public delegate void ConfigureContext<TViewModel>(TViewModel viewModel);
+
+	public interface IDialogService
+	{
+		void ShowDialog<TViewModel>(DialogCallback? callback = null, ConfigureContext<TViewModel>? configure = null)
+			where TViewModel : DialogViewModel;
+	}
+}
---
+++ Synergy.WPF.Navigation/ViewModels/DialogViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Synergy.WPF.Navigation.ViewModels
+{
+	public partial class DialogViewModel : ViewModel
+	{
+		[ObservableProperty]
+		private bool? _dialogResult;
+
+		public virtual void ConfigureWindow(System.Windows.Window window) { }
+	}
+}
