d29162c82d1320e05f500fbc8e6fe95e7f5b1716
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-07-09 20:16:29 +0200

    Fixing bugs.

--- Synergy.WPF.Navigation/Managers/NavigationManager.cs
+++ Synergy.WPF.Navigation/Managers/NavigationManager.cs
@@ -7,11 +7,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Synergy.WPF.Navigation.Managers
 {
-	public class NavigationManager
+	public class NavigationManager(IServiceProvider services)
 	{
+		#region Nested classes
+
 		private class FrameAttacher(
 			Action<UserControlFrame> attach,
 			Action<UserControlFrame> detach)
@@ -28,15 +32,24 @@ namespace Synergy.WPF.Navigation.Managers
 			public UserControlFrameVM? FrameVM { get; set; }
 		}
 
-		private readonly IServiceProvider _services;
+		#endregion
 
+		private readonly IServiceProvider _services = services;
 		private readonly ConcurrentDictionary<object, NavigationChannel> _channels = [];
 
-		public NavigationManager(IServiceProvider services)
-		{
-			_services = services;
-		}
+		private static Dispatcher? Dispatcher => Application.Current?.Dispatcher;
 
+		/// <summary>
+		/// Attaches specified navigation service to manager channel,
+		/// or create new if not exists, via key.
+		/// If there is attached view with the same key, that view and
+		/// navigation service will be connected.
+		/// </summary>
+		/// <param name="key">Channel key.</param>
+		/// <param name="navigationService">Navigation service.</param>
+		/// <exception cref="InvalidOperationException">If some navigation service as already attached
+		/// with this key, then you need to detach it first.
+		/// </exception>
 		internal void Attach(object key, INavigationService navigationService)
 		{
 			var frameVM = new UserControlFrameVM(navigationService, _services);
@@ -61,12 +74,16 @@ namespace Synergy.WPF.Navigation.Managers
 					existing.Frame = frame;
 					existing.FrameVM = frameVM;
 
-					existing.Attacher?.Attach(frame);
+					Dispatcher?.Invoke(() => existing.Attacher?.Attach(frame));
 
 					return existing;
 				});
 		}
 
+		/// <summary>
+		/// Detach navigation channel from manager. It detaches both navigation service and view.
+		/// </summary>
+		/// <param name="key">Channel key.</param>
 		internal void Detach(object key)
 		{
 			if (!_channels.TryRemove(key, out var channel))
@@ -74,13 +91,21 @@ namespace Synergy.WPF.Navigation.Managers
 
 			if (channel.Frame is not null)
 			{
-				channel.Attacher?.Detach(channel.Frame);
+				Dispatcher?.Invoke(() => channel.Attacher?.Detach(channel.Frame));
 				channel.Frame.DataContext = null;
 			}
 
 			channel.FrameVM?.Dispose();
 		}
 
+		/// <summary>
+		/// Attaches view, where <see cref="UserControlFrame"/> will be put.
+		/// <paramref name="detach"/> must actually detach <see cref="UserControlFrame"/> from your view!
+		/// </summary>
+		/// <param name="key">Channel key.</param>
+		/// <param name="attach">Attaching delegate.</param>
+		/// <param name="detach">Detaching delegate.</param>
+		/// <exception cref="InvalidOperationException"></exception>
 		public void Attach(
 			object key,
 			Action<UserControlFrame> attach,
@@ -102,7 +127,7 @@ namespace Synergy.WPF.Navigation.Managers
 					existing.Attacher = new(attach, detach);
 
 					if (existing.Frame is not null)
-						existing.Attacher.Attach(existing.Frame);
+						Dispatcher?.Invoke(() => existing.Attacher.Attach(existing.Frame));
 
 					return existing;
 				});
